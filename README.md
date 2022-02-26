dd6e16c424573377460cfc3ac70ebe6cfe11b0d2
Davide Miceli <dauwak@gmail.com>
2022-02-26 19:30:37 +0100

    #1 Add starting landing page.

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# bridge-training-engine
-A Free and Open Source platform for Bridge training.
+<p align="center">
+  <a href="https://github.com/davidemiceli/bridge-training-engine" target="_blank" rel="noopener noreferrer">
+    <img src="assets/img/logo.png" alt="Bridge Training Engine">
+  </a>
+</p>
+<h2 align="center">Bridge Training Engine</h2>
+<p align="center"><em>A Free and Open Source platform for Bridge training.</em></p>
+
+## Getting Started
+
+### Requirements
+
+- Node.js v16.14+
+
+### Run application
+
+To start the application, run:
+
+```shell
+# Install FE JavaScript requirements
+npm install
+# Build FE application
+npm run generate
+# Run server application
+npm run dev
+```
+
+Then open the url http://localhost:3000 on browser.
+
+### Development
+
+For frontend application development, follow the commands below.
+
+```bash
+# install dependencies
+$ npm install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm run start
+
+# generate static project
+$ npm run generate
+```
---
+++ nuxt.config.js
@@ -0,0 +1,67 @@
+export default {
+  // Build only client-side (https://go.nuxtjs.dev/config-ssr)
+  ssr: false,
+  // Target (https://go.nuxtjs.dev/config-target)
+  target: 'static',
+
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    title: 'Bridge Training Engine',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'A Free and Open Source platform for Bridge training.' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Staatliches&display=swap' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;500;600;700&display=swap' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,400;1,500;1,700;1,900&display=swap' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons|Material+Icons+Outlined' }
+    ],
+    script: [
+    ],
+  },
+
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: [
+    '@/node_modules/bulma/css/bulma.min.css',
+    '@/assets/css/custom.css'
+  ],
+
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [
+  ],
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: [
+    {
+      path: '~/components', // will get any components nested in let's say /components/test too
+      pathPrefix: false,
+    }
+  ],
+
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [
+  ],
+
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [
+    // https://go.nuxtjs.dev/bootstrap
+    '@nuxtjs/toast',
+  ],
+
+  toast: {
+    position: 'top-right',
+    duration: 2000
+  },
+
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {
+  },
+
+  loading: '@/components/loadingBar.vue'
+
+}
